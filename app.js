0949b530fc07777f7340918f8f556ffcbaf8fe07
jimist <arash.jeem@gmail.com>
2018-12-16 22:43:38 +0330

    project start

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
