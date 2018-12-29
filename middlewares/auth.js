4804f84aadbe3fef18ec8d516677e6d908db41a4
jimist <arash.jeem@gmail.com>
2018-12-29 16:05:44 +0330

    authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,9 @@
 var auth_user = function (req, res, next) {
-  if (true)
-    next()
-  else
-      res.status(403).send('Forbidden')
+    let userToken = req.cookies['user-token']
+    if(userToken=='arash'){
+        next()
+    }
+    res.redirect('/account/login?type=failed')
 }
 
 var auth_admin = function (req, res, next) {
