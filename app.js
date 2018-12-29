122a3c0cbd3bddcfcaf3daf70c4be488eae1de4d
jimist <arash.jeem@gmail.com>
2018-12-29 16:06:30 +0330

    fix in main app and controllers

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let path = require('path');
 let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
 
 let app = express()
 let port = 3000 //process.env.PORT || 3000
+
+app.use(cookieParser())
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 let auth = require('../middlewares/auth');
 auth = auth.auth_user
-// router.use('/comments', require('./comments'))
-// router.use('/users', require('./users'))
+
+router.use('/account', require('./account'))
 
 router.get('/', auth, (req, res, next) => {
   res.render('index', {title: 'arash'})
-});
-router.get('/login', auth, (req, res, next) => {
-  res.render('pages/login', {title: 'arash'})
 });
 
 module.exports = router
