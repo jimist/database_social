11b5d8bbeaec558eece18030d3b1c16d12300b0e
jimist <arash.jeem@gmail.com>
2019-01-30 04:06:34 +0330

    final touches and changes, some admin features are added

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,8 +4,54 @@ const router = express.Router()
 let auth = require('../middlewares/auth');
 auth = auth.auth_admin;
 
-router.get('/', auth, (req, res, next) => {
-    res.send('tests')
+router.get('/gossip_users', auth, (req, res, next) => {
+    let getUsersWithHighCommentsPerPost = `
+        select * from (select tt.user_id as user_id from
+        (
+        SELECT user_id as user_id, count(*) as comment_per_post FROM posts 
+        where type='comment'
+        group by reply_to, user_id
+        having count(id)>9
+        )tt group By user_id
+        having count(tt.user_id)>2)T,  users as U
+        where U.id=T.user_id
+    `;
+
+    db.query(getUsersWithHighCommentsPerPost, (err, result) => {
+       if(result.length>0){
+           res.render('users/list', {users: result});
+       } else {
+           res.send('no such users!')
+       }
+    });
+});
+
+router.get('/fake_users', auth, (req, res, next) => {
+
+    let fakeLikeGetListQuery = `
+    select * from (select t1.actor_id as user_id
+    from (select l.user_id as actor_id, a.user_id as target_id, count(*) as total_likes
+    from likes as l, posts as a
+    where l.post_id=a.id
+    group by actor_id, target_id)t1, (
+    select l.user_id as actor_id, a.user_id as target_id, count(*) as total_likes
+    from likes as l, posts as a
+    where l.post_id=a.id
+    group by actor_id
+    )t2 where t1.actor_id=t2.actor_id and t1.total_likes>=(t2.total_likes/2)
+    )T, users as U
+    where U.id=T.user_id and (U.posts_count / ((NOW() - U.created_at)/86400))<0.1
+    group by T.user_id
+    `;
+
+    db.query(fakeLikeGetListQuery, (err, result) => {
+        if(result.length>0){
+            res.render('users/list', {users: result});
+        }else{
+            res.send('no such user found!')
+        }
+
+    });
 });
 
 module.exports = router;
--- controllers/analysor.js
+++ controllers/analysor.js
@@ -4,4 +4,85 @@ const router = express.Router()
 let auth = require('../middlewares/auth');
 auth = auth.auth_analysor;
 
+router.get('/list/follow_back', auth, (req, res, next) => {
+    let followBackCountQuery = `
+    SELECT COUNT(*) as count, r1.target_id as user_id
+    FROM relationships as r1, relationships as r2
+    WHERE r1.type='follow' and r2.type='follow'
+    and r2.target_id=r1.actor_id and r1.target_id=r2.actor_id
+    GROUP BY r1.target_id 
+    HAVING count>0
+    `;
+
+    db.query(followBackCountQuery, (err, result) => {
+       if(result.length>0){
+           usersFollowBackCounts = {};
+           let userIdsToCheck = [];
+           result.forEach((res) => {
+               usersFollowBackCounts[res.user_id] = res.count;
+               userIdsToCheck.push(res.user_id);
+           });
+           db.query(`SELECT * FROM users WHERE id IN (${userIdsToCheck.join(',')})`, (err, result) => {
+               let usersToShow = [];
+               result.forEach((tmpUser) => {
+                    if(usersFollowBackCounts[tmpUser.id]==tmpUser.followers_count){
+                        usersToShow.push(tmpUser);
+                    }
+               });
+               res.render('users/list', {users: usersToShow})
+           });
+       }else{
+           res.send('no body has followed back anybody!')
+       }
+    });
+});
+
+router.get('/list/elite_users', auth, (req, res, next) => {
+    let getAllWhoPostEachDay = `
+        select T.user_id as user_id from 
+        (
+        select user_id, count(*) as days_count from
+        (
+        SELECT user_id as user_id, count(*) as post_count
+        FROM posts 
+        GROUP BY user_id, round(created_at/86400)
+        HAVING count(*)>0
+        )tt
+        GROUP BY tt.user_id
+        )T, users as U
+        WHERE T.user_id=U.id and round((NOW()-U.created_at)/86400)<=T.days_count
+    `;
+
+    db.query(getAllWhoPostEachDay, (err, result) => {
+        if(result.length>0){
+            let usersList = [];
+            result.forEach((val) => {
+               usersList.push(val.user_id);
+            });
+
+
+            let getUsersWithValidFollwingsCount = `
+                select * from (
+                SELECT actor_id as actor_id, count(*) as valid_follow_count FROM relationships
+                where type='follow' and target_id IN (${usersList.join(',')})
+                GROUP BY actor_id
+                )T, users as U where T.actor_id=U.id and T.valid_follow_count=U.followings_count
+            `;
+
+            db.query(getUsersWithValidFollwingsCount, (err, result) => {
+               if(result.length>0){
+                   res.render('users/list', {users: result});
+               } else{
+                   res.send('no user who only follows users with one post every day!');
+               }
+            });
+        }else{
+            res.send("no user who post every day!");
+        }
+    });
+
+
+
+});
+
 module.exports = router;
